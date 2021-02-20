68da116725310d51f1549a76e90ff8cd4020b9a7
EdgarSabidoC <a12004194@alumnos.uady.mx>
2021-02-19 22:46:47 -0600

    Se simplifico el archivo rs

--- Subsequence/Subsequence.rs
+++ Subsequence/Subsequence.rs
@@ -1,12 +1,8 @@
-use std::io::{stdin, stdout, Read, Write};
-
 fn main() {
   let mut arreglo = [5, 1, 22, 25, 6, -1, 8, 10];
   let mut secuencia = [1, 6, -1, 10];
   
   println!("{}", es_sub_secuencia(&mut arreglo, &mut secuencia));
-
-  pause();
 }
 
 fn es_sub_secuencia(arreglo: &mut[i32], secuencia: &mut[i32]) -> bool{
@@ -25,11 +21,4 @@ fn es_sub_secuencia(arreglo: &mut[i32], secuencia: &mut[i32]) -> bool{
   }
 
   return false;
-}
-
-fn pause() {
-  let mut stdout = stdout();
-  stdout.write(b"Press <Enter> to continue...").unwrap();
-  stdout.flush().unwrap();
-  stdin().read(&mut [0]).unwrap();
 }
